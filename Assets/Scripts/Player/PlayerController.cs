6da45ed5f926aaf1fefc12c0d5da5bbdbdd4acfb
Michael <pulis.michael@gmail.com>
2019-11-05 11:24:06 -0700

    Added boost pad and player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace EAE.Race.Player
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Controls the player
+    /// </summary>
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //public modifiable variables
+        public float BoardSpeed = 3.0f;
+        public float RotateSpeed = 30.0f;
+        public float BoostMod = 2.0f;
+
+
+
+        //private gameplay variables
+        private bool racing = false;
+
+        private bool movingLeft = false;
+        private bool movingRight = false;
+
+        private bool boosting = false;
+
+
+        #region Setup
+        private void Awake()
+        {
+            racing = true;
+        }
+
+        #endregion Setup
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!racing)
+                return;
+
+            if (movingLeft || Input.GetKey(KeyCode.A))
+            {
+                //transform.Translate(Vector3.left * boardSpeed * Time.deltaTime);
+                transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+            }
+            else if (movingRight || Input.GetKey(KeyCode.D))
+            {
+                //transform.Translate(Vector3.right * boardSpeed * Time.deltaTime);
+                transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+            }
+
+            if (!boosting)
+                transform.Translate(Vector3.forward * BoardSpeed * Time.deltaTime);
+            else
+                transform.Translate(Vector3.forward * BoardSpeed * BoostMod * Time.deltaTime);
+
+        }
+
+
+        #region Boost
+        /// <summary>
+        /// Starts the boost for the player
+        /// </summary>
+        public void StartBoost()
+        {
+            boosting = true;
+        }
+
+        /// <summary>
+        /// Ends the boost for the player
+        /// </summary>
+        public void EndBoost()
+        {
+            boosting = false;
+        }
+        #endregion Boost
+
     }
 }
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EAE.Race.Player
+{
+    /// <summary>
+    /// Holds all player data such as time played and points
+    /// </summary>
+    public class PlayerData : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
