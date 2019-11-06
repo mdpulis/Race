d1d3c953afbbb2a8ac9825b1d7607bde1124112d
Josh Nelson <josh.nelson1@yahoo.com>
2019-11-06 13:08:58 -0700

    Added background music. Added utility script class.

---
+++ Assets/Scripts/Utility/Util.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{    
+    public static T RandomItem<T>(this List<T> list)
+    {
+        if (list.Count == 0)
+            return default(T);
+        if (list.Count == 1)
+            return list[0];        
+        return list[(Random.Range(0, list.Count))];
+    }
+}
