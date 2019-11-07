46c4404a04ca497375d72bb8e492770c429e0602
Josh Nelson <josh.nelson1@yahoo.com>
2019-11-07 13:10:47 -0700

    GyroControls connected to player controller

--- Assets/Scripts/Input/PhoneMotionControls.cs
+++ Assets/Scripts/Input/PhoneMotionControls.cs
@@ -11,14 +11,20 @@ namespace EAE.Race.InputMethods
         public float deadzone;
 
         private Quaternion deviceAttitude;
+        private Vector3 deviceOrigin;
 
         private PlayerController pc; 
+
 
         // Start is called before the first frame update
         void Start()
         {
             pc = GetComponent<PlayerController>();
-            Debug.Log(SystemInfo.supportsGyroscope);
+            if(SystemInfo.supportsGyroscope)
+            {
+                Input.gyro.enabled = true;
+            }
+            deviceOrigin = Input.gyro.attitude.eulerAngles;
         }
 
         // Update is called once per frame
@@ -29,10 +35,24 @@ namespace EAE.Race.InputMethods
         }
 
         void processOrientation()
-        {
-            Debug.Log(deviceAttitude);
+        {            
             Vector3 euler = deviceAttitude.eulerAngles;
-            Debug.Log("x:" + euler.x + " y:" + euler.y + " z:" + euler.z);
+            // Debug.Log("x:" + euler.x + " y:" + euler.y + " z:" + euler.z);
+            float rotationDeltaZ = Mathf.Abs(euler.z - deviceOrigin.z);
+            if(rotationDeltaZ>deadzone)
+            {
+                pc.setMovingLeft(false);
+                pc.setMovingRight(true);
+            }else if(rotationDeltaZ<deadzone)
+            {
+                pc.setMovingLeft(true);
+                pc.setMovingRight(false);
+            }
+            else
+            {
+                pc.setMovingLeft(false);
+                pc.setMovingRight(false);
+            }
         }
     }
 
